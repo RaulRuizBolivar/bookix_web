367bbec7932ab82a06050380f8ed327f1037329c
Raúl Ruiz <raul.ruiz.bolivar@gmail.com>
2022-07-26 18:44:50 +0200

    Cierro sesion

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 
 @Component( {
@@ -13,14 +14,26 @@ export class PerfilComponent implements OnInit {
   bookClubAdmin: any
   subscriptions: any
   constructor (
-    private userService: UserService
-  ) { }
+    private userService: UserService,
+    private router: Router
+  ) {
+    this.cargarDatos()
+    // this.userService.getUser().then( user => this.user = user )
+    // this.userService.getHistorial().then( historial => this.historial = historial )
+    // this.userService.getBookClubAdmin().then( bookClubAdmin => this.bookClubAdmin = bookClubAdmin )
+    // this.userService.getSubscriptions().then( subscriptions => this.subscriptions = subscriptions )
+  }
 
-  async ngOnInit () {
+  ngOnInit () { }
+  async cargarDatos () {
     this.user = await this.userService.getUser()
     this.historial = await this.userService.getHistorial()
     this.bookClubAdmin = await this.userService.getBookClubAdmin()
     this.subscriptions = await this.userService.getSubscriptions()
+  }
+  cerrarSesion () {
+    localStorage.removeItem( 'user-token' )
+    this.router.navigate( [ '/login' ] )
   }
 
 }
